36659636ee0e33e88396748a728a1d207d96bd20
carl hand <carl.a.hand92@gmail.com>
2020-09-11 14:50:38 +0100

    moving model code into it's own file

---
+++ server/booklopedia/models.py
@@ -0,0 +1,20 @@
+from booklopedia import db, ma
+from datetime import datetime
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(20))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __init__(self, title, author, category):
+        self.title = title
+        self.author = author
+        self.category = category
+
+
+class BookSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'author', 'category', 'date_created')
