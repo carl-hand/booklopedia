79a81be17a97141f42524ed4ecaaf747558c708d
carl hand <carl.a.hand92@gmail.com>
2020-09-11 14:52:46 +0100

    using standard convention for creating python app to allow for easy creation of multiple environments

---
+++ server/booklopedia/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, redirect, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from booklopedia.config import Config
+
+db = SQLAlchemy()
+ma = Marshmallow()
+# mysql://scott:tiger@localhost/mydatabase
+
+
+def create_app(config_class=Config):
+    """Construct the core application."""
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # have to import routes here because they import app variable
+    from booklopedia.books.routes import books
+    app.register_blueprint(books)
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    return app
+
+    # with app.app_context():
+    #     from __main__ import routes  # Import routes
+    #     db.create_all()  # Create sql tables for our data models
+
+    #     return app
