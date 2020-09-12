a708039233c3fd139178a9f8dc8400a912cefec9
carl hand <carl.a.hand92@gmail.com>
2020-09-12 10:53:44 +0100

    updating paths

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_marshmallow import Marshmallow
 from datetime import datetime
 
 
-app = Flask(__name__, static_folder='client/build', static_url_path="")
+app = Flask(__name__, static_folder='client/build',
+            template_folder="client/build", static_url_path="")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -34,7 +35,7 @@ books_schema = BookSchema(many=True)
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 
 @app.route("/books", methods=["GET"])
