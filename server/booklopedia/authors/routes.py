db1ad13c1c7a9d305f3e2e4c9b736cc024a0c19a
carl hand <carl.a.hand92@gmail.com>
2020-09-14 07:18:40 +0100

    added author routes

---
+++ server/booklopedia/authors/routes.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, Blueprint
+from server.booklopedia.authors.models import Author, AuthorSchema
+
+authors = Blueprint("authors", __name__)
+
+author_schema = AuthorSchema()
+authors_shchema = AuthorSchema(many=TRUE)
+
+
+@authors.route("/authors", methods=["GET"])
+def get_authors():
+    all_authors = Author.query.all()
+    return authors_schema.jsonify(all_authors)
+
+
+@authors.route("/author/<id>", methods=["GET"])
+def get_author(id):
+    author = Author.query.get_or_404(id)
+    return author_shchema.jsonify(author)
