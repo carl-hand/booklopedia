59ff6dc39e0578d84fb7a5fd9b999819c9602a15
carl hand <carl.a.hand92@gmail.com>
2020-09-14 09:26:39 +0100

    fixing cors issue and rendering data from db on the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const result = await axios.get("http://127.0.0.1:5000/books");
+      const data = result.data;
+
+      setBooks(data);
+      console.log(data);
+    };
+
+    getBooks();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {books.map((book) => {
+            return <li>{book.title}</li>;
+          })}
+        </ul>
       </header>
     </div>
   );
--- server/booklopedia/__init__.py
+++ server/booklopedia/__init__.py
@@ -1,25 +1,35 @@
-from flask import Flask, request, redirect, jsonify, abort, send_from_directory, render_template
+from flask import (
+    Flask,
+    request,
+    redirect,
+    jsonify,
+    abort,
+    send_from_directory,
+    render_template,
+)
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from .config import Config
 
 db = SQLAlchemy()
 ma = Marshmallow()
-# mysql://scott:tiger@localhost/mydatabase
 
 
 def create_app(config_class=Config):
     """Construct the core application."""
-    app = Flask(__name__, static_folder='../../client/build',
-                static_url_path="")
+    app = Flask(__name__, static_folder="../../client/build", static_url_path="")
+    CORS(app)
     app.config.from_object(Config)
+
     # have to import routes here because they import app variable
     from server.booklopedia.books.routes import books
+
     app.register_blueprint(books)
 
-    @app.route('/')
+    @app.route("/")
     def index():
-        return app.send_static_file('index.html')
+        return app.send_static_file("index.html")
 
     db.init_app(app)
     ma.init_app(app)
