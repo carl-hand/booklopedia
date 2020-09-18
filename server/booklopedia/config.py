44b24404d38894e9aa28fc017f52e95cbde2647c
carl hand <carl.a.hand92@gmail.com>
2020-09-18 09:26:57 +0100

    moved api key into config file

--- server/booklopedia/config.py
+++ server/booklopedia/config.py
@@ -3,23 +3,29 @@ from os import environ, path
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+load_dotenv(path.join(basedir, ".env"))
 
 
 class Config:
     """Set Flask configuration from .env file."""
 
     # General Config
-    SECRET_KEY = environ.get('SECRET_KEY')
-    FLASK_APP = environ.get('FLASK_APP')
-    FLASK_ENV = environ.get('FLASK_ENV')
+    SECRET_KEY = environ.get("SECRET_KEY")
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_ENV = environ.get("FLASK_ENV")
 
     # Database
-    DB_USER = environ.get('DB_USER')
-    DB_PASSWORD = environ.get('DB_PASSWORD')
-    DB_SERVER = environ.get('DB_SERVER')
-    DB_NAME = environ.get('DB_NAME')
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(
-        user=DB_USER, password=DB_PASSWORD, server=DB_SERVER, database=DB_NAME)
+    DB_USER = environ.get("DB_USER")
+    DB_PASSWORD = environ.get("DB_PASSWORD")
+    DB_SERVER = environ.get("DB_SERVER")
+    DB_NAME = environ.get("DB_NAME")
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+mysqlconnector://{user}:{password}@{server}/{database}".format(
+            user=DB_USER, password=DB_PASSWORD, server=DB_SERVER, database=DB_NAME
+        )
+    )
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # API
+    API_KEY = environ.get("API_KEY")
