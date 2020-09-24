6777090cf1e011673c363490eda1fb6c611827cf
carl hand <carl.a.hand92@gmail.com>
2020-09-24 07:50:17 +0100

    specifying property name for trackingId

--- client/src/utils/analyticUtils.js
+++ client/src/utils/analyticUtils.js
@@ -1,6 +1,6 @@
 import ReactGA from "react-ga";
 
 export const logPageView = () => {
-  ReactGA.initialize(process.env.GA_TRACKING_ID);
+  ReactGA.initialize({ trackingId: process.env.GA_TRACKING_ID });
   ReactGA.pageview("/");
 };
